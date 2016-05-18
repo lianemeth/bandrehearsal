9111dd471eff11abe3e21393f123a8850b488e23
Lucas Nemeth <lucas.nemeth@gmail.com>
2016-05-18 16:26:43 +0100

    add tests

---
+++ bandrehearsal/views/json_data.py
@@ -0,0 +1,9 @@
+from ..models import User
+from pyramid.view import view_config
+
+@view_config(context=User, name='events' renderer="json") 
+def get_user_events(request, context):
+    events = {}
+    for event in context.events:
+        events[event] = format_event(event)
+    return events
